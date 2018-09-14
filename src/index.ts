63d76fd653e9e3b224b443a9fcbd8ef73ebf1c62
Keigo Yamamoto <k5.trismegistus@gmail.com>
2018-09-14 10:36:14 +0900

    :cry: Importing has performance problem... Started fixing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import * as Express from 'express';
+require('dotenv').config()
 
+import * as Express from 'express';
+import { Db, DbCreateOptions } from 'mongodb'
+
+import { connectDb } from './repository'
 import { initApi } from './api'
 
 const app = Express();
 
-initApi(app)
+connectDb().then((db: Db) => {
+  initApi(app, db)
 
-app.listen(
-    3001,
-    () => {
-        console.log('Example app listening on port 3001!');
-    });
+  app.listen(
+      3001,
+      () => {
+          console.log('Example app listening on port 3001!');
+      });
+})
+
 
 export default app;
