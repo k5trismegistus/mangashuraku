7f94820965f1d41ff195ef1213603bf786b88bfb
Keigo Yamamoto <k5.trismegistus@gmail.com>
2018-10-17 00:48:35 +0900

    :construction_worker: CircleCI config

--- src/__tests__/requests/health.test.ts
+++ src/__tests__/requests/health.test.ts
@@ -1,6 +1,18 @@
 require('dotenv').config()
+import * as http from 'http'
 import * as request from 'supertest'
 import { app } from '../../app'
+
+let server: http.Server
+
+beforeAll((done) => {
+  server = http.createServer(app)
+  server.listen(done)
+})
+
+afterAll((done) => {
+  server.close(done)
+})
 
 describe('GET /api/helath', () => {
   it('should return status 200', async () => {
